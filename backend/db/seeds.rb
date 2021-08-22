fc942c8a2843637484c01b84f38995facd10a28f
Andrew <andrew.casin@gmail.com>
2021-08-21 23:59:16 -0500

    Create seed file data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+post_a = Post.create(image: "Test image 1", caption: "This is a test 1", hashtag: "#test1")
+post_b = Post.create(image: "Test image 2", caption: "This is a test 2", hashtag: "#test2")
+post_c = Post.create(image: "Test image 3", caption: "This is a test 3", hashtag: "#test3")
+
+
+upvote_a = Upvote.create(upvote_count: 1, post_id: 1)
+upvote_b = Upvote.create(upvote_count: 2, post_id: 2)
+upvote_c = Upvote.create(upvote_count: 3, post_id: 3)
+
+
