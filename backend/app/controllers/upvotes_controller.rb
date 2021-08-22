814309e706eea9daa8becffebd0880f29530029a
Andrew <andrew.casin@gmail.com>
2021-08-22 01:24:42 -0500

    Add actions to Upvotes Controller

--- backend/app/controllers/upvotes_controller.rb
+++ backend/app/controllers/upvotes_controller.rb
@@ -1,2 +1,39 @@
 class UpvotesController < ApplicationController
+    # before_action :set_post, only: [:show, :destroy]
+
+    #all upvotes
+    # def index
+
+    # end
+
+    # #show one post
+    # def show
+
+    # end
+
+    # #create new post
+    # def create
+
+    # end
+
+    #update post
+    # def update
+
+    # end
+
+    # #delete post
+    # def destroy
+
+    # end
+
+    # private
+    # # Use callbacks to share common setup or constraints between actions.
+    # def set_post
+    #     post = Post.find(params[:id])
+    # end
+
+    # #only allow a list of trusted parameters through
+    # def post_params
+    #     params.require(:category).permit(:image, :caption, :hashtag)
+    # end    
 end
