a7933eec444f5e9dc526697860fffd6fa08272da
Andrew <andrew.casin@gmail.com>
2021-08-22 01:11:33 -0500

    Create index and show for posts controller

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,24 +1,37 @@
 class PostsController < ApplicationController
 
+    #all posts
     def index
-
+        @posts = Post.all
+        
+        render json: @posts
     end
 
+    #show one post
     def show
-
+        render json: @post
     end
 
+    #create new post
     def create
 
     end
 
+    #update post
     def update
 
     end
 
+    #delete post
     def destroy
 
     end
 
+    private
+
+    #only allow a list of trusted parameters through
+    def post_params
+
+    end
 
 end
