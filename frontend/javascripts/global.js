087c37b4bcbf09ed27472351006c0272ba30966a
Andrew <andrew.casin@gmail.com>
2021-08-23 01:15:23 -0500

    Add variables to global.js

---
+++ frontend/javascripts/global.js
@@ -0,0 +1,11 @@
+//list of variables
+const baseUrl = "http://localhost:3000"
+const navbar =  () => document.getElementsByClassName('navbar')
+const logo = () => document.getElementById('logo')
+const feed = () => document.getElementById('feed')
+const post = () => document.getElementById('post')
+const postForm = () => document.getElementById('post-form')
+const postImage = () => document.getElementById('post-image')
+const postCaption = () => document.getElementById('post-caption') 
+const postHashtag = () => document.getElementById('post-hashtag')
+const upvoteButton = () => document.getElementById('upvotebutton')
