a526e412d08242ee5c9b9496668cf51ae0fc9c67
Andrew <andrew.casin@gmail.com>
2021-08-23 01:43:00 -0500

    Updates to serializers

--- backend/app/serializers/post_serializer.rb
+++ backend/app/serializers/post_serializer.rb
@@ -1,3 +1,4 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :image, :caption
+  has_many :upvotes, except [:created_at, :updated_at]
 end
--- backend/app/serializers/upvote_serializer.rb
+++ backend/app/serializers/upvote_serializer.rb
@@ -1,3 +1,4 @@
 class UpvoteSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :post_id, :upvote_count
+  belongs_to :post, except: [:created_at, :updated_at]
 end
