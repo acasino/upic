b3a3963ac86f9732dc6922eb38d3973455e554cc
Andrew <andrew.casin@gmail.com>
2021-08-24 02:14:05 -0500

    Continue building handleSubmit in postApi

--- frontend/javascripts/services/postApi.js
+++ frontend/javascripts/services/postApi.js
@@ -2,6 +2,21 @@ class postApi {
 
 
     //handle submit
+    static handleSubmit(e) {
+        e.preventDefault()
+
+        //retrieve file from file input
+        const file = e.target['image'].files[0]
+        const caption = e.target['caption'].value
+        const hashtag = e.target['hashtag'].value
+        const formData = new FormData()
+        //append to formData
+        formData.append('image', file)
+        formData.append('caption', caption)
+        formData.append('hashtag', hashtag)
+        
+
+    }
 
 
 
