179940480b0424eaadc6330360ffe3887490be64
Andrew <andrew.casin@gmail.com>
2021-08-24 22:16:00 -0500

    Add skip_before_action in PostsController

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-    protect_from_forgery
+    # protect_from_forgery
+    skip_before_action :verify_authenticity_token
     before_action :set_post, only: [:show, :update, :destroy]
 
     #all posts
