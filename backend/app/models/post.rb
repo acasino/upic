0656671da45c2125ddfd71e300699c9491febb13
Andrew <andrew.casin@gmail.com>
2021-08-26 01:59:41 -0500

    Updates to environment production and dev

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -1,8 +1,16 @@
 class Post < ApplicationRecord
     include Rails.application.routes.url_helpers
     
-    has_many :upvotes
     has_one_attached :image
+    has_many :upvotes
     
+    validates :image, {
+        presence: true
+    }
+
+    def get_image_url
+        url_for(self.image)
+    end
+
 
 end
