1dec215353c6461b1482f7a1f8624286dcc2d56b
Andrew <andrew.casin@gmail.com>
2021-08-26 02:53:03 -0500

    Build out PostSerializer

--- backend/app/serializers/post_serializer.rb
+++ backend/app/serializers/post_serializer.rb
@@ -1,4 +1,16 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :image, :caption
   has_many :upvotes, except: [:created_at, :updated_at]
+
+  def posts
+    self.object.posts.map do |p|
+      {
+        id: p.id, 
+        image: p.image,
+        caption: p.caption
+      }
+    end
+  end
+
+
 end
