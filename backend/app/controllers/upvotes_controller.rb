d6868a411372435020ac6df6b9dacfd74cae956d
Andrew <andrew.casin@gmail.com>
2021-09-02 23:39:32 -0500

    Create upvotecontroller index, show, create

--- backend/app/controllers/upvotes_controller.rb
+++ backend/app/controllers/upvotes_controller.rb
@@ -2,19 +2,25 @@ class UpvotesController < ApplicationController
     # before_action :set_post, only: [:show, :destroy]
 
     #all upvotes
-    # def index
+    def index
+        upvotes = Upvote.all
+        render json: upvotes
+    end
 
-    # end
-
-    # #show one post
-    # def show
-
-    # end
+    def show
+        render json: upvote
+    end
 
     # #create new post
-    # def create
+    def create
+        upvote = Upvote.new(upvote_params)
 
-    # end
+        if upvote.save
+            render json: upvote, status: :created, location: upvote
+        else 
+            render json: upvote.errors, status: :unprocessable_entity
+        end
+    end
 
     #update post
     # def update
