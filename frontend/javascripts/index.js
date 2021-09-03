1161630ddb81d5b85b7d1276b6a37fa6f947a59d
Andrew <andrew.casin@gmail.com>
2021-09-02 23:21:37 -0500

    Update to global.js

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //listen for form submit
     postApi.fetchPosts()
     postForm().addEventListener("submit", postApi.handleSubmit)
+    
 })
 
 
