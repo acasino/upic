38daaf994f605c9845a63b14f03087487514f368
Andrew <andrew.casin@gmail.com>
2021-09-07 02:31:33 -0500

    Create after_add under Post model

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -2,7 +2,11 @@ class Post < ApplicationRecord
     include Rails.application.routes.url_helpers
     
     has_one_attached :image
-    has_many :upvotes, :dependent => :destroy
+    has_many :upvotes, :dependent => :destroy, :after_add => :create_upvote
+
+    def create_upvote(upvote)
+        self.upvote.create(id: self.id)
+    end
     
     # validates :image, {
     #     presence: true
