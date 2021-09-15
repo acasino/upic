f1d834159079df0a04280a97c651d0481b2f7f41
Andrew <andrew.casin@gmail.com>
2021-09-15 00:40:28 -0500

    Delete unused code

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -1,7 +1,6 @@
 //index.js
 
 document.addEventListener('DOMContentLoaded', () => {
-    //listen for form submit
     postApi.fetchPosts()
     upvoteApi.fetchUpvotes()
     postForm().addEventListener("submit", postApi.handleSubmit)
